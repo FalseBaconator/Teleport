71c3c5b25c798578b1659cc416c507f45f098aca
Ryan Wohler <ryanwohler55@gmail.com>
2023-01-19 14:46:02 -0400

    First Playable

---
+++ Assets/Scripts/TeleportTrigger.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TeleportTrigger : MonoBehaviour
+{
+    public string PlayerTag;
+    public Transform target;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == PlayerTag)
+        {
+            Debug.Log("Before|| Target: " + target.position + " Player: " + other.transform.position);
+            other.GetComponent<CharacterController>().enabled = false;
+            other.transform.position = target.position;
+            other.GetComponent<CharacterController>().enabled = true; 
+            Debug.Log("After|| Target: " + target.position + " Player: " + other.transform.position);
+        }
+    }
+
+}
