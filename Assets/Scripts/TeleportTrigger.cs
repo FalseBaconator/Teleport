97d2ae468e8efb91bdf511362d11d16ffadf2117
Ryan Wohler <ryanwohler55@gmail.com>
2023-01-19 15:48:51 -0400

    Test teleport while walking
    Testing what happens when you press teleport when walking should show that Matt should have the same problems as you

--- Assets/Scripts/TeleportTrigger.cs
+++ Assets/Scripts/TeleportTrigger.cs
@@ -4,17 +4,27 @@ using UnityEngine;
 
 public class TeleportTrigger : MonoBehaviour
 {
-    public string PlayerTag;
+    //public string PlayerTag;
     public Transform target;
+    public GameObject player;
+
+    private int timesPorted = 0;
 
     private void OnTriggerEnter(Collider other)
     {
-        if(other.tag == PlayerTag)
+
+        if(other.gameObject.CompareTag(player.tag))
         {
             //Debug.Log("Before|| Target: " + target.position + " Player: " + other.transform.position);
-            other.GetComponent<CharacterController>().enabled = false;
-            other.transform.position = target.position;
-            other.GetComponent<CharacterController>().enabled = true; 
+            //other.GetComponent<CharacterController>().enabled = false;
+            //other.transform.position = target.position;
+            //other.GetComponent<CharacterController>().enabled = true; 
+
+            //Vector3 toMove = target.position - other.transform.position;
+            Vector3 toMove = target.position - player.transform.position;
+            Debug.Log(toMove.magnitude + "||" + Time.time);
+            player.GetComponent<CharacterController>().Move(toMove);
+
             //Debug.Log("After|| Target: " + target.position + " Player: " + other.transform.position);
         }
     }
